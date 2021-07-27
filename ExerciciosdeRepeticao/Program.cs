42064ebc7e55fb63f400f9957d3bbf966aa9a4f4
victorabreu10 <victordeabreu2011@hotmail.com>
2021-07-26 23:25:24 -0300

    Exercicio com for

---
+++ ExerciciosdeRepeticao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExerciciosdeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Media dos alunos");
+
+            float soma = 0;
+            float media = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite a nota do aluno" + i + ":");
+                float nota = 0;
+                float.TryParse(Console.ReadLine(), out nota);
+                soma += nota;
+            }
+            media = soma / 5;
+            Console.WriteLine("Média é igual a:" + media);
+        }
+    }
+}
